02eeee3961526c4de8b8a592fa0de1dee154cbc1
Mambo <47549774+MamboYT@users.noreply.github.com>
2020-03-16 10:17:34 -0700

    Added Skirmish project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 10f;
+
+    private Vector2 movement;
+
+    void OnMove(InputValue value) 
+    {
+        Debug.Log("Move");
+
+        movement = value.Get<Vector2>();
+
+        rb.AddForce(transform.right * 5000 * movement.x);
+    }
+
+    void OnJump()
+    {
+        Debug.Log("Jump");
+
+        rb.AddForce(transform.up * 25000);
+    }
+
+}
